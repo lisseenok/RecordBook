e81d3b65fb73b0146cdd0a1f553bf0974e8fcfbd
lisseenok <45551611+lisseenok@users.noreply.github.com>
2022-09-19 22:49:24 +0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.html"],
+  theme: {
+    // colors: {
+    //   'some_red': '#B22222',
+    //   'my_white': '#fffbeb',
+    // },
+    extend: {
+      fontFamily: {
+        'nunito': ['"Nunito"']
+      }
+    },
+  },
+  plugins: [],
+}
